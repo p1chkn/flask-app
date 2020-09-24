3bc05b1aaaf65ee43efe39facd571b07a1212c0e
Pavel Chuykin <p7chkn@yandex.ru>
2020-09-24 18:21:13 +0700

    add flask app

---
+++ hello.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.rout('/')
+def hello_world():
+    return "I'm done it!"
+
+
+if __name__ = '__main__':
+    app.run(debug=True, host='0.0.0.0')
